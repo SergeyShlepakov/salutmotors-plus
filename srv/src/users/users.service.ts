00159452196a961e9d655be04fd8c9890a534c1d
Sergey V. Shlepakov <shles@ya.ru>
2024-04-17 22:02:10 +0300

    Task #4 Изменил возвращаемое значение с полного списка на частичный с возможностью указать страницу списка.
    Номер запрашиваемой страницы указывается в параметре запроса `page`, например, `/users?page=2`.
    
    Ответ содержит 20 элементов списка в поле `users` и общее количество элементов списка в поле `count`.
    
    Количество возвращаемых элементов регулируется константой `take` класса `UserController`.
    
    Обратная совместимость не сохранена для упрощения решения, так как это не продакшен версия.

--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -16,4 +16,13 @@ export class UserService {
   async findAll(): Promise<UsersEntity[]> {
     return await this.usersRepo.find();
   }
+
+  /**
+   * Returns an array containing a list of items and the number of all items in the list.
+   * @param take
+   * @param skip
+   */
+  async findAndCount(take: number, skip: number): Promise<[UsersEntity[], number]> {
+    return await this.usersRepo.findAndCount({ take: take, skip: skip });
+  }
 }
